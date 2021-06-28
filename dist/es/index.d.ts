203ab309ad74b6e1fcf4ed4acef350b1c825eb45
黄婷婷 <huangtingting3@C15252.local>
2021-06-28 11:51:48 +0800

    wip: add gitigore

--- dist/es/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import './index.scss';
-declare const RollupDemo: () => JSX.Element;
-export { RollupDemo };
--- dist/es/utils.d.ts
+++
@@ -1,8 +0,0 @@
-declare const testArray: (array: number[]) => {
-    id: number;
-    name: number;
-}[];
-declare const a = "nameA";
-declare const b = "nameB";
-declare const funcA: () => void;
-export { testArray, funcA, a, b };
